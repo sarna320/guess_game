0a3b4d7a090f46a16665184582353ff1c3db6c83
sarna320 <sarnacki99@wp.pl>
2023-11-27 23:42:11 +0100

    done

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import random
 
+
 def guess(number):
-    if number==number_to_guess:
+    if number == number_to_guess:
         print("Your guess is correct, you won")
         return True
     else:
-        if number>number_to_guess:
+        if number > number_to_guess:
             print("To high")
-        elif number<number_to_guess:
+        elif number < number_to_guess:
             print("To low")
     print("Guess again")
     return False
@@ -17,7 +18,7 @@ print("Welcom to the Number Guessing Game!")
 print("I am thinking of a number between 1 and 100")
 difficulty = input("Chose a difficluty. Type 'easy' or 'hard': ")
 
-live=1
+live = 1
 if difficulty == "hard":
     live = 5
 elif difficulty == "easy":
@@ -25,11 +26,11 @@ elif difficulty == "easy":
 
 number_to_guess = random.randint(1, 100)
 
-while(live>0):
+while live > 0:
     if guess(int(input("Make a guess: "))):
         break
     else:
-        live-=1
+        live -= 1
         print(f"You have {live} attempts remaing to guess")
-    if(live==0):
+    if live == 0:
         print("You have run out of guess, you lose")
